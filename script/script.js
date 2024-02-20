f7c7841055fa43ae2429f07ee08d970e4f657c96
AkashKApoor <akashkapoor306@gmail.com>
2024-02-20 12:35:16 -0500

    Updated the UI made basic functionality

--- script/script.js
+++ script/script.js
@@ -1,10 +1,110 @@
 const buttons = document.querySelectorAll('.btn');
-function buttonValue(e)
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('.equal');
+const clearButton = document.querySelector('.clear');
+const display = document.querySelector('.display');
+
+let firstNumber = 0;
+let operator = "";
+let secondNUmber = 0;
+
+function sum(num1,num2)
 {
-    const display = document.querySelector('.display');
-    display.textContent = this.textContent;
-    num = this.textContent;
-    console.log(this.textContent);
+    return num1 + num2;
 }
 
-buttons.forEach(button => button.addEventListener('click',buttonValue));
+function subtract(num1,num2)
+{
+
+    return num1 - num2;
+}
+
+function multiply(num1,num2)
+{
+    return num1 * num2
+}
+
+function divide(num1,num2)
+{
+    return num1/num2;
+}
+
+
+function calulationOperator(operator,num1,num2)
+{
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    switch(operator)
+    {
+        case "+":
+            display.textContent = sum(num1,num2);
+            console.log(sum(num1,num2));
+            break
+        case "-":
+            display.textContent = subtract(num1,num2);
+            console.log(subtract(num1,num2));
+            break;
+        case "*":
+            display.textContent = multiply(num1,num2);
+            console.log(multiply(num1,num2));
+            break;
+        case "/":
+            display.textContent = divide(num1,num2);
+            console.log(divide(num1,num2));
+            break;
+        case "%":
+            display.textContent = 'Method implemention In progress';
+            break;
+        default:
+            console.log("No operator Selected");
+            break;
+    }
+    firstNumber = "";
+    secondNUmber = "";
+    operator ="";
+}
+
+
+
+function buttonValve(e)
+{
+    if(!operator)
+    {
+        firstNumber += this.textContent;
+        display.textContent = firstNumber;
+        console.log(firstNumber);
+    }
+    else
+    {   
+        secondNUmber += this.textContent;
+        display.textContent = secondNUmber;
+        console.log(secondNUmber);
+    }
+   
+}
+
+function operatorSelect(e)
+{
+    operator = this.textContent;
+    display.textContent = operator
+    console.log(operator);
+}
+
+function resetValues() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    display.textContent = '';
+    
+}
+
+
+buttons.forEach(button => button.addEventListener('click',buttonValve));
+operators.forEach(operator => operator.addEventListener('click',operatorSelect));
+clearButton.addEventListener('click',resetValues)
+equal.addEventListener('click',function(){
+    calulationOperator(operator,firstNumber,secondNUmber)
+
+});
+
